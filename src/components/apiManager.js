5646231c7cb83bad328599c43464b6ded85ce15d
Chris Abbondandelo <abbondcj@gmail.com>
2022-06-20 19:17:50 -0400

    added api manager for json and finished login feature

---
+++ src/components/apiManager.js
@@ -0,0 +1,4 @@
+export const usersApi = "http://localhost:9000/users"
+export const tripsApi = "http://localhost:9000/trips"
+export const participantsApi = "http://localhost:9000/participants"
+export const fishApi = "http://localhost:9000/fish"

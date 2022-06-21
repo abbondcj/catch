a7b1941da313491db39d4d29d6e3194231e75372
Chris Abbondandelo <abbondcj@gmail.com>
2022-06-20 20:59:34 -0400

    did minimal styling for nav and set up correct links

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -8,21 +8,20 @@ export const NavBar = () => {
   return (
     <ul className="navbar">
         <li className="navbar__item">
+            <Link className="navbar__link" to="/my-trips">My Trips</Link>
+        </li>
+
+        <li className="navbar__item">
             <Link className="navbar__link" to="/plan-trip">Plan Trip</Link>
         </li>
 
         <li className="navbar__item">
-            <Link className="navbar__link" to="/my-trips">My Trips</Link>
-        </li>
-
-        {/* <li className="navbar_item">
-          <Link className="navbar_link"  to="/my-fish">My Fish</Link>
-        </li> */}
+          <Link className="navbar__link"  to="/my-fish">My Fish</Link>
+        </li> 
 
         <li className="navbar__item">
             <Link className="navbar__link" to="/my-profile">My Profile</Link>
         </li>
-
         <li className="navbar__item">
             <Link className="navbar_link" to="/login"
                 onClick={
